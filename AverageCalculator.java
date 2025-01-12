c65daa30e29de1c29cdd3ed599bb7fa438e4f7a0
Toleen_AbuAdi <toleeno003@gmail.com>
2025-01-12 21:16:02 +0300

    edit the average class

--- AverageCalculator.java
+++ AverageCalculator.java
@@ -1,24 +1,28 @@
 import java.util.Scanner;
 public class AverageCalculator {
     int[] marks;
-
+    int numberOfStudents;
     public void inputMarks() {
         Scanner read = new Scanner(System.in);
-        marks = new int[30];
-        System.out.println("Enter the marks of 30 students: ");
-        for (int i = 0; i < 30; i++) {
+        System.out.println("Enter the number of students: ");
+        numberOfStudents = read.nextInt();
+
+        marks = new int[numberOfStudents];
+        System.out.println("Enter the marks of students: ");
+        for (int i = 0; i < numberOfStudents; i++) {
             marks[i] = read.nextInt();
         }
+        
         read.close();
     }
 
     public void calculateAverage() {
         System.out.println("Calculating average...");
         int sum = 0;
-        for (int i = 0; i < 30; i++) {
+        for (int i = 0; i < numberOfStudents; i++) {
             sum += marks[i];
         }
-        System.out.println("Average: " + sum / 30);
+        System.out.println("Average: " + sum / numberOfStudents);
     }
 
     public static void main(String[] args) {
