8850cba0d9d4d5e1282721b343a5c123a70567dc
Toleen_AbuAdi <toleeno003@gmail.com>
2025-01-12 21:02:18 +0300

    adding the Main class

--- Main.java
+++ Main.java
@@ -1,1 +1,8 @@
-
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Average Calculator!");
+        AverageCalculator averageCalculator = new AverageCalculator();
+        averageCalculator.inputMarks();
+        averageCalculator.calculateAverage();
+    }
+}
